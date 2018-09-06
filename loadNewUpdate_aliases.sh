fe9a704d6bcda9a73a44f2b8298d0a5cab16224d
Helpi_Stone <defjam9007@gmail.com>
2018-09-06 13:54:55 +0200

    add loadnewUpdate_aliases.sh

---
+++ loadNewUpdate_aliases.sh
@@ -0,0 +1,16 @@
+
+###
+# Filename:          loadNewUpdate_aliases.sh
+# Author:            Helpi
+# Date:              2018-09-06
+# Version:           1.0
+# -----
+# Last Modified: Thu Sep 06 2018
+# Modified By: Helpi
+# 
+###
+
+
+# Datum : 2018-09-06
+curl https://raw.githubusercontent.com/Defjam121/.bash_aliases/master/update_aliases.sh > /scripts/update_aliases.sh
+echo "Data update"
